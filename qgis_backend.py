07b1923b03afc33a8c4930d02613f99b54730a57
K.R.Schuur <57346737+KRS-dev@users.noreply.github.com>
2020-05-29 16:15:45 +0200

    Update qgis_backend.py

--- qgis_backend.py
+++ qgis_backend.py
@@ -464,7 +464,7 @@ class QgisBackend:
                             colnames = [desc[0] for desc in description]
                             trx_result_df.columns = colnames
                             trx_result_df[['EA', 'COH', 'FI']] = trx_result_df[[
-                                'EA', 'COH', 'FI']].apply(pd.to_numeric).replace({'0':np.nan, 0:np.nan})
+                                'EA', 'COH', 'FI']].apply(pd.to_numeric).replace({0: np.nan})
                             df_list.append(trx_result_df)
                     trx_result_df_all = pd.concat(df_list, ignore_index=True)
                     if trx_result_df_all.empty is False:
@@ -509,7 +509,7 @@ class QgisBackend:
                             colnames = [desc[0] for desc in description]
                             trx_dlp.columns = colnames
                             trx_dlp.loc[:, 'EPS50':] = trx_dlp.loc[:,
-                                'EPS50':].apply(pd.to_numeric).replace({'0':np.nan, 0:np.nan})
+                                'EPS50':].apply(pd.to_numeric).replace({0: np.nan})
                             df_list.append(trx_dlp)
                     trx_dlp_all = pd.concat(df_list, ignore_index=True)
                     if trx_dlp_all.empty is False:
@@ -556,7 +556,7 @@ class QgisBackend:
                             trx_dlp_result.rename(
                                 columns={'TPR_EA': 'EA'}, inplace=True)
                             trx_dlp_result.loc[:, 'EA':] = trx_dlp_result.loc[:, 'EA':].apply(
-                                pd.to_numeric).replace({'0':np.nan, 0:np.nan})
+                                pd.to_numeric).replace({0: np.nan})
                             df_list.append(trx_dlp_result)
                     trx_dlp_result_all = pd.concat(df_list, ignore_index=True)
                     if trx_dlp_result_all.empty is False:
@@ -777,7 +777,7 @@ class QgisBackend:
                             sdp_df.columns = colnames
                             sdp_df.loc[:, 'VOLUMEGEWICHT_DROOG':] = \
                                 sdp_df.loc[:, 'VOLUMEGEWICHT_DROOG':].apply(
-                                    pd.to_numeric).replace({0:np.nan})
+                                    pd.to_numeric).replace({0: np.nan})
                             df_list.append(sdp_df)
                     sdp_df_all = pd.concat(df_list, ignore_index=True)
                     if sdp_df_all.empty is False:
@@ -823,7 +823,7 @@ class QgisBackend:
                             sdp_result_df.columns = colnames
                             sdp_result_df.loc[:, 'LOAD':] = \
                                 sdp_result_df.loc[:, 'LOAD':].apply(
-                                    pd.to_numeric).replace({0:np.nan})
+                                    pd.to_numeric).replace({0: np.nan})
                             df_list.append(sdp_result_df)
                     sdp_result_df_all = pd.concat(df_list, ignore_index=True)
                     if sdp_result_df_all.empty is False:
