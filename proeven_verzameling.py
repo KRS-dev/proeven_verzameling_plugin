a228a8737345edf6031060a23cf5be4dde725677
K.R.Schuur <57346737+KRS-dev@users.noreply.github.com>
2020-06-17 17:17:13 +0200

    Update proeven_verzameling.py

--- proeven_verzameling.py
+++ proeven_verzameling.py
@@ -998,8 +998,8 @@ class ProevenVerzamelingTask(QgsTask):
                         break
                     oldload = load
             
-            step2 = pd.concat(step2_list, 1).T
-            herbelast = pd.concat(herbelast_list, 1).T
+            step2 = pd.concat(step2_list, 1)
+            herbelast = pd.concat(herbelast_list, 1)
             df_dict.update({
                 'step2': step2,
                 'herbelast_stap': herbelast
